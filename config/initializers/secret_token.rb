441c2e03a3be10f50ab12c2e41d71f567e976b90
theanhhpp <theanhhpp@gmail.com>
2015-09-21 20:22:41 +0700

    the anh

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheanhFirstapp::Application.config.secret_key_base = '11b8b553887d94bb52c459ee105caf218ad5a385661736ba45525a758c67a1c429d004186370e688ac8fb2fbd0d7933b72b38804dceb17f0eb63d90e47789167'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheanhFirstapp::Application.config.session_store :cookie_store, key: '_theanh_firstapp_session'
